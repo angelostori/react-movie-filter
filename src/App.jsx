c33d9e7bcad8ee5cbed7b522ac0fc0fa5ce1c6bc
angelostori <angelostori13@gmail.com>
2025-11-11 11:37:01 +0100

    define an initial list of movies and its initial state

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 
 function App() {
 
+  const initialMovies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
 
+  const [movies, setMovies] = useState(initialMovies)
 
   return (
     <>
